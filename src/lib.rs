ee790ac5710ca980fa90eae8421d7b0f63d8d08a
andrew <>
2022-12-03 01:11:03 +0900

    Ordered serialization of maps

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-use serde::{Deserialize, Serialize};
+use std::collections::{BTreeMap, HashMap};
+
+use serde::{Deserialize, Serialize, Serializer};
 
 pub mod projectconfig;
 
@@ -7,4 +9,15 @@ pub struct AccountId(pub i32);
 
 pub fn is_default<T: Default + PartialEq>(t: &T) -> bool {
     t == &T::default()
+}
+
+pub fn serde_order_map<K: Ord + Serialize, V: Serialize, S: serde::Serializer>(
+    value: &HashMap<K, V>,
+    serializer: S,
+) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let ordered: BTreeMap<_, _> = value.iter().collect();
+    ordered.serialize(serializer)
 }
--- src/projectconfig/v1.rs
+++ src/projectconfig/v1.rs
@@ -2,14 +2,22 @@ use std::collections::HashMap;
 
 use serde::{Deserialize, Serialize};
 
-use crate::{is_default, AccountId};
+use crate::{is_default, serde_order_map, AccountId};
 
 #[derive(Clone, Serialize, Deserialize, Default)]
 #[serde(deny_unknown_fields, rename_all = "snake_case")]
 pub struct Weights {
-    #[serde(default, skip_serializing_if = "HashMap::is_empty")]
+    #[serde(
+        default,
+        skip_serializing_if = "HashMap::is_empty",
+        serialize_with = "serde_order_map"
+    )]
     pub accounts: HashMap<AccountId, u32>,
-    #[serde(default, skip_serializing_if = "HashMap::is_empty")]
+    #[serde(
+        default,
+        skip_serializing_if = "HashMap::is_empty",
+        serialize_with = "serde_order_map"
+    )]
     pub projects: HashMap<String, u32>,
 }
 
