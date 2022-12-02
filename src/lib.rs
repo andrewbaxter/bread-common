4f79ba2fa90e17c6189e1821ac533dbb3e533a6a
andrew <>
2022-12-02 22:14:14 +0900

    Missed files

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use serde::{Deserialize, Serialize};
 
 pub mod projectconfig;
@@ -9,13 +7,4 @@ pub struct AccountId(pub i32);
 
 pub fn is_default<T: Default + PartialEq>(t: &T) -> bool {
     t == &T::default()
-}
-
-#[derive(Clone, Serialize, Deserialize, Default)]
-#[serde(deny_unknown_fields, rename_all = "snake_case")]
-pub struct Weights {
-    #[serde(default, skip_serializing_if = "HashMap::is_empty")]
-    pub accounts: HashMap<AccountId, u32>,
-    #[serde(default, skip_serializing_if = "HashMap::is_empty")]
-    pub projects: HashMap<String, u32>,
 }
