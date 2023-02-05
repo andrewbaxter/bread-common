2914bf79fd41e20c172629354ff0e459de531139
andrew <>
2023-02-05 23:35:56 +0900

    Updates

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,39 @@
-use std::collections::{BTreeMap, HashMap};
-
-use serde::{Deserialize, Serialize, Serializer};
+use std::collections::{
+    BTreeMap,
+    HashMap,
+};
+use serde::{
+    Deserialize,
+    Serialize,
+    Serializer,
+};
 
 pub mod projectconfig;
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Hash, Serialize, Deserialize)]
-pub struct AccountId(pub i32);
+pub struct AccountId(pub i64);
+
+impl AccountId {
+    pub fn from_sql(x: i64) -> Result<Self, String> {
+        Ok(AccountId(x))
+    }
+
+    pub fn to_sql(self) -> i64 {
+        self.0
+    }
+}
 
 pub fn is_default<T: Default + PartialEq>(t: &T) -> bool {
     t == &T::default()
 }
 
-pub fn serde_order_map<K: Ord + Serialize, V: Serialize, S: serde::Serializer>(
-    value: &HashMap<K, V>,
-    serializer: S,
-) -> Result<S::Ok, S::Error>
+pub fn serde_order_map<
+    K: Ord + Serialize,
+    V: Serialize,
+    S: serde::Serializer,
+>(value: &HashMap<K, V>, serializer: S) -> Result<S::Ok, S::Error>
 where
-    S: Serializer,
-{
+    S: Serializer {
     let ordered: BTreeMap<_, _> = value.iter().collect();
     ordered.serialize(serializer)
 }
--- src/projectconfig/mod.rs
+++ src/projectconfig/mod.rs
@@ -1,6 +1,10 @@
-use serde::{Deserialize, Serialize};
+use serde::{
+    Deserialize,
+    Serialize,
+};
 
 pub mod v1;
+
 pub mod latest {
     pub use super::v1::*;
 }
@@ -9,6 +13,16 @@ pub mod latest {
 #[serde(deny_unknown_fields, rename_all = "snake_case")]
 pub enum VersionedProjectConfig {
     V1(v1::Config),
+}
+
+impl VersionedProjectConfig {
+    pub fn from_sql(x: Vec<u8>) -> Result<Self, serde_json::Error> {
+        Ok(serde_json::from_slice(&x)?)
+    }
+
+    pub fn to_sql(&self) -> Vec<u8> {
+        serde_json::to_vec(self).unwrap()
+    }
 }
 
 pub const FILENAME: &'static str = ".bread.yml";
